c5a975c0ec30eb03f65e0b8144453e68a04457fb
Adam Stitou <adamkoora27@gmail.com>
2024-08-31 17:59:53 +0100

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 import HomePage from "./components/pages/home";
 import About from "./components/sections/About";
 import Navigation from "./components/navigation/Navigation";
+import { Analytics } from "@vercel/analytics/react";
 import Projects from "./components/sections/Projects";
 import Contact from "./components/sections/Contact";
 
 const App = () => {
   return (
     <>
+      <Analytics />
       <Navigation />
       <Routes>
         <Route path="/" element={<HomePage />} />
